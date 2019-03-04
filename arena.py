7c235dfc5480547d0ba8a10971b03595f98af7ec
Alex AMADORI <aamadori@student.42.fr>
2019-03-04 22:26:58 +0100

    separated python files

---
+++ arena.py
@@ -0,0 +1,68 @@
+import random
+import os
+import re
+import itertools
+import threading
+import champion
+
+class Arena:
+    fight_semaphore = threading.BoundedSemaphore(value=8)
+    @staticmethod
+    def fight(first, second, fight_map=("resources/maps/map01", 984)):
+        first.compile()
+        second.compile()
+        output = os.popen("resources/filler_vm -q -p1 %s -p2 %s -f %s" % (first.get_filename(), second.get_filename(), fight_map[0]))
+        pattern = re.compile("== ([OX]) fin: ([0-9]+)")
+        for line in output:
+            match = pattern.match(line)
+            if match is not None:
+                if match.group(1) == "O":
+                    first_score = match.group(2)
+                elif match.group(1) == "X":
+                    second_score = match.group(2)
+        # TODO error if first_score or second score are None?
+        first.matches += 1
+        second.matches += 1
+        first.score += float(first_score) / fight_map[1]
+        second.score += float(second_score) / fight_map[1]
+        return [int(first_score), int(second_score)]
+    @staticmethod
+    def fight_worker(first, second, maps, map_weights, verbose = False):
+        Arena.fight_semaphore.acquire()
+        map = random.choices(maps, map_weights)[0]
+        print("Players %s and %s in map %s, fight!" % (first.get_basename(), second.get_basename(), map[0]))
+        Arena.fight(first, second, map)
+        Arena.fight_semaphore.release()
+    def __init__(self, initial_population = 30):
+        self.initial_population = initial_population
+        fixed_filenames = [
+            "champely.filler",
+            "hcao.filler",
+            "mnishimo.filler",
+            "carli.filler",
+            "jaelee.filler",
+            "superjeannot.filler"
+        ]
+        self.fix_champions = [champion.FixedChampion(filename) for filename in fixed_filenames]
+        self.dyn_champions = [champion.DynamicChampion() for i in range(self.initial_population)]
+        self.champions = self.fix_champions + self.dyn_champions
+    def measure_fitness(self, dyn_tries = 210):
+        maps = [
+            ("resources/maps/map01", 984),
+            ("resources/maps/map02", 10000)
+        ]
+        matches = random.sample(list(itertools.combinations(self.dyn_champions, 2)), dyn_tries)
+        threads = list()
+        for opponents in matches:
+            th = threading.Thread(target=Arena.fight_worker,
+                args=(opponents[0], opponents[1], maps, [0.85, 0.15], True))
+            th.start()
+            threads.append(th)
+        fixed_matches = list(itertools.product(self.dyn_champions, self.fix_champions))
+        for opponents in fixed_matches:
+            th = threading.Thread(target=Arena.fight_worker,
+                args=(opponents[0], opponents[1], maps, [0, 1], True))
+            th.start()
+            threads.append(th)
+        for th in threads:
+            th.join()
---
+++ champion.py
@@ -0,0 +1,60 @@
+import random
+import os
+
+class Champion:
+    def __init__(self, basename):
+        self.basename = basename
+        self.matches = 0
+        self.score = 0
+    def get_fitness(self):
+        return self.score / self.matches
+    def get_basename(self):
+        return self.basename
+    def compile(self):
+        return
+    def recompile(self):
+        return
+    def is_dynamic(self):
+        return False
+
+class FixedChampion(Champion):
+    def __init__(self, basename):
+        super().__init__(basename)
+    def get_filename(self):
+        return "resources/players/" + self.basename
+
+class DynamicChampion(Champion):
+    global_id = 0
+    def __init__(self):
+        self.id = DynamicChampion.global_id + 1
+        DynamicChampion.global_id = DynamicChampion.global_id + 1
+        super().__init__("champion%d.filler" % self.id)
+        self.opponent_decay = random.random()
+        self.self_decay = random.random() * 2 - 1
+        self.wall_decay = random.random() * 2 - 1
+        self.penetration_bonus = random.random() * 10 - 5
+        self.wall_bonus = random.random() * 2 - 1
+        self.updated = False
+    def get_filename(self):
+        return "pool/" + self.get_basename()
+    def get_sourcename(self):
+        return "pool/params/champion%d.c" % self.id
+    def output_file(self):
+        file = open(self.get_sourcename(), "w+")
+        file.write("const float g_op_decay = %f;\n" % self.opponent_decay)
+        file.write("const float g_me_decay = %f;\n" % self.self_decay)
+        file.write("const float g_wall_decay = %f;\n" % self.wall_decay)
+        file.write("const float g_penetration_bonus = %f;\n" % self.penetration_bonus)
+        file.write("const float g_wall_bonus = %f;\n" % self.wall_bonus)
+        file.write("const char *g_champion_name = \"%s\";\n" % self.get_basename())
+        file.close()
+    def compile(self):
+        if not self.updated:
+            self.output_file()
+            os.system("make " + self.get_filename())
+        self.updated = True
+    def recompile(self):
+        self.updated = False
+        self.compile()
+    def is_dynamic(self):
+        return True

a86438091d712596c06f6370e2a30c58b838af37
Alex Amadori <aamadori@42.student.fr>
2019-03-05 12:26:22 +0100

    Fixed bedroom class

--- bedroom.py
+++ bedroom.py
@@ -5,42 +5,41 @@ import itertools
 from copy import deepcopy
 
 class Bedroom:
-    def __init__(self, mutation_rate=0.04, crossover_rate=0.80, extinction_rate=0.40, elitism=1):
+    def __init__(self, mutation_rate=0.02, crossover_rate=0.80, extinction_rate=0.50, elitism=2):
         self.mutation_rate = mutation_rate
         self.crossover_rate = crossover_rate
         self.extinction_rate = extinction_rate
         self.elitism = elitism
     def mutate(self, champions):
         for ch in champions:
-            inverted_bits = random.sample(range(len(ch.chromosome)), int(self.mutation_rate * 32 * 5))
-            for index in inverted_bits:
-                ch.chromosome[index] = True if ch.chromosome[index] else False
+            mutated_genes = random.sample(ch.chromosome, int(self.mutation_rate * len(ch.chromosome)))
+            for index in range(len(mutated_genes)):
+                mutated_genes[index] = False if mutated_genes[index] else True
     def crossover(self, champions, target):
         children = list()
-        for i in range(target):
-            parents = random.sample(champions, 2)
+        combs = list(itertools.combinations(champions, 2))
+        for parents in random.sample(combs, target):
             cross_point = random.choice(range(len(parents[0].chromosome)))
-            new_chromosome = deepcopy(parents[0].chromosome[:cross_point]) + deepcopy(parents[1].chromosome[cross_point:])
-            children.append(champion.DynamicChampion(new_chromosome))
+            recombined = parents[0].chromosome[:cross_point] + parents[1].chromosome[cross_point:]
+            children.append(champion.DynamicChampion(recombined))
         return children
-    def reproduce(self, old_champions):
-        champions = deepcopy(old_champions)
-        original_length = len(champions)
-        champions.sort(reverse=True)
-        #kill off worse champions
-        extinct = int(self.extinction_rate * len(champions))
-        champions = champions[:extinct]
+    def reproduce(self, original_pool):
+        pool = deepcopy(original_pool)
+        pool.sort(reverse=True)
+        # kill off low end
+        pool = pool[:int((1.0 - self.extinction_rate) * len(pool))]
         offspring = list()
-        #apply elitism
-        offspring.extend(champions[:self.elitism])
-        champions = champions[self.elitism:]
-        #apply mutations
-        self.mutate(champions)
-        #apply crossover
-        random.shuffle(champions)
-        crossed = champions[:int(self.crossover_rate * len(champions))]
-        champions = champions[int(self.crossover_rate * len(champions)):]
-        #pass the remaining champions along
-        offspring.extend(champions)
-        offspring.extend(self.crossover(crossed, original_length - len(offspring)))
+        # pass along elitists
+        for ch in pool[:self.elitism]:
+            offspring.append(champion.DynamicChampion(ch.chromosome))
+        # apply mutation
+        self.mutate(pool[self.elitism:])
+        # pass along non cross over'd champions
+        random.shuffle(pool)
+        for ch in pool[int(self.crossover_rate * len(pool)):]:
+            offspring.append(champion.DynamicChampion(ch.chromosome))
+        pool = pool[:int(self.crossover_rate * len(pool))]
+        target_length = len(original_pool) - len(offspring)
+        offspring.extend(self.crossover(pool, target_length))
         return offspring
+
